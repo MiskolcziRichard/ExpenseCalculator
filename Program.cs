644c603a0a107aa3f7815a320b3a127de63cc3f0
Miskolczi Richárd <richardmiskolczi10@gmail.com>
2019-09-08 19:04:25 +0200

    Starting from scratch. What can I say. I was new to programming when I started this project and the previous code wasn't just smelly, it was visually rotten and infected with all kinds of nasty stuff. I reviewed it recently and it was the worst code I have ever seen in my life. I just couldn't let that thing be on my GitHub page.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace Actual_Expense_Calculator
+{
+    class Program
+    {
+        #region Static Fields
+        public static int Balance {get; set;}
+        #endregion
+
+        static class SaveFile
+        {
+          public void Create()
+          {
+            File saveFile = new File("./.save");
+            saveFile.Close();
+          }
+
+          public void Erase()
+          {
+
+          }
+
+          public void Save()
+          {
+
+          }
+
+          public void Load()
+          {
+
+          }
+        }
+
+        class Expense
+        {
+          protected int Value {get; set;}
+          protected int Name {get; set;}
+          protected Date Date {get; set;}
+
+          public Expense()
+          {
+            Console.Write("Enter the name of the new expense: ");
+            this.Name = Console.ReadLine();
+
+            Console.Write("Enter the value of the expense: ");
+            this.Value = Console.ReadLine();
+          }
+        }
+
+        class Constant : Expense
+        {
+
+        }
+
+        class OneTime : Expense
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
