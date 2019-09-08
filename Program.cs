88d9e29e48283fc3dd05ba0c8274116e8c71dcb9
Miskolczi Richárd <richardmiskolczi10@gmail.com>
2019-09-08 20:23:54 +0200

    Things are coming together, what can I say. I made progress. - Created a static class 'FileManagment' which is filled with methods to help file managment, such as saving changes, loading from save file etc. - Created base class 'Expense' which holds two attributes, one being the value (price) and the other the name of the expense. It also has a constructor which takes input in order to fill in these attributes. - Created class derivered from 'Expense' called 'Constant'. Instances of this class will represent expenses that ought to be automatically decremented from the user's balance (whether bank or cash is a matter of the user's preferences) every x day (also specified by the user) - Created class derivered from 'Expense' called 'OneTime'. These are -as the name suggests- one time purchases such as buying a smart phone for $X at a specified date (specified by user) - Created static struct 'Balance' which holds two attributes, one representing the user's bank account balance and the other being the user's cash amount - Created static method 'Introduction' which takes input from the user to give 'Balance.Cash' and 'Balance.Card' their values

--- Program.cs
+++ Program.cs
@@ -6,10 +6,41 @@ namespace Actual_Expense_Calculator
     class Program
     {
         #region Static Fields
-        public static int Balance {get; set;}
+        static struct Balance
+        {
+          public int Cash {get; set;}
+          public int Card {get; set;}
+        }
         #endregion
 
-        static class SaveFile
+        static void Introduction()
+        {
+          Console.WriteLine("Hello!\n");
+          Thread.Sleep(1000);
+
+          Console.WriteLine("We would like to ask for some of your financial informations.");
+          Thread.Sleep(1000);
+
+          do
+          {
+            try
+            {
+              Console.Write("What is your account balance?: ");
+              Balance.Card = Convert.ToInt32(Console.ReadLine());
+
+              Console.Write("What is your cash amount?: ");
+              Balance.Cash = Convert.ToInt32(Console.ReadLine());
+              break;
+            } catch (FormatException e)
+            {
+              Console.WriteLine("You can only enter numbers!");
+              FileManagment.LogError(e);
+              continue;
+            }
+          } while (true);
+        }
+
+        static class FileManagment
         {
           public void Create()
           {
@@ -29,7 +60,32 @@ namespace Actual_Expense_Calculator
 
           public void Load()
           {
+            File save = new File("./.save");
 
+            if (!save.Exists)
+            {
+              Introduction();
+            }
+          }
+
+          public void LogError(Exception e)
+          {
+            do
+            {
+              try
+              {
+                using (StreamWriter w = File.AppendText("./.errorLog"))
+                {
+                  w.WriteLine(e + "\n");
+                }
+                break;
+              }
+              catch (FileNotFoundException)
+              {
+                File errorLog = new File("./.errorLog");
+                continue;
+              }
+            } while (true);
           }
         }
 
@@ -37,7 +93,6 @@ namespace Actual_Expense_Calculator
         {
           protected int Value {get; set;}
           protected int Name {get; set;}
-          protected Date Date {get; set;}
 
           public Expense()
           {
@@ -51,12 +106,64 @@ namespace Actual_Expense_Calculator
 
         class Constant : Expense
         {
+          private int Interval {get; set;}
 
+          public Constant()
+          {
+            private int Interval {get; set;}
+            Console.Write("");
+          }
         }
 
         class OneTime : Expense
         {
+          private DateTime PurchaseDate {get; set;}
 
+          public OneTime()
+          {
+            do
+            {
+              Console.Write("Did you make this purchase today? (y/n): ");
+
+              try
+              {
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+                break;
+              } catch (FormatException e)
+              {
+                Console.WriteLine("You need to enter either 'y' or 'n' as an answer!");
+
+                FileManagment.LogError(e);
+                continue;
+              }
+
+            } while (true);
+
+            if (userInput == 'y')
+            {
+              this.PurchaseDate = DateTime.Today;
+            }
+            else
+            {
+              Console.WriteLine("Please specify the date of the purchase in the following format:");
+
+              do
+              {
+                try
+                {
+                  Console.Write("dd/MM/yyyy: ");
+                  this.PurchaseDate = Convert.ToDateTime(Console.ReadLine());
+
+                  break;
+                } catch (FormatException e)
+                {
+                  Console.WriteLine("You need to enter the date in the specified format!");
+                  FileManagment.LogError(e)
+                  continue;
+                }
+              } while (true);
+            }
+          }
         }
 
         static void Main(string[] args)
